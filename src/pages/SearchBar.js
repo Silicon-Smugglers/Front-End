af35fc021363568af4bceea0f3d0a3c06535df01
GoodGameRuler <yaboiudit@gmail.com>
2023-08-27 08:59:53 +1000

    Add modal

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import "./css/SearchBar.css"
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: "10px",
+    height: "80vh",
+    width: "80vw",
+    padding: "0" 
+  },
+};
 
 const SearchBar = ({ searchQuery, setSearchQuery, onSearch }) => {
   // const [searchQuery, setSearchQuery] = useState('');
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
@@ -11,6 +28,14 @@ const SearchBar = ({ searchQuery, setSearchQuery, onSearch }) => {
   const handleSearch = () => {
     onSearch(searchQuery);
   };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div className='SearchContainer'>
@@ -22,7 +47,29 @@ const SearchBar = ({ searchQuery, setSearchQuery, onSearch }) => {
         style={{ marginRight: "15px" }}
       />
       {/* <button className='SearchContainerButton' onClick={handleSearch}>Search</button> */}
-      <button className='SearchContainerButton'>Finish</button>
+      <button className='SearchContainerButton' onClick={openModal}>Finish</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className='results'>
+          <div className='header'>
+            <h2>Results</h2>
+
+          </div>
+            <div className='known-issues'>
+              <h2>Known Issues</h2>
+            </div>
+            <div className='potential-risks'>
+              <h2>Potential Risks</h2>
+            </div>
+            <button onClick={closeModal} className='SearchContainerButton'>close</button>
+
+        </div>
+        
+      </Modal>
     </div>
   );
 };
