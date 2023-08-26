9577db23844784c8724aafc6393dabd71a19ee28
SillySuli <sulavmalla.malla7@gmail.com>
2023-08-27 03:07:05 +1000

    chage pill logo and icon

--- src/components/ColourChangingButton.js
+++ src/components/ColourChangingButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+{/*npm install class-variance-authority react tailwind-merge*/}
 export function ColorChangingButton({
   component: Component = 'button',
   children,
@@ -12,7 +13,7 @@ export function ColorChangingButton({
       className={`p-4 rounded-lg border transition-all duration-300 ease-in-out ${
         buttonProps.pressed
           ? 'bg-white border-red-500 text-red-500 font-semibold text-lg'
-          : 'bg-red-500 border-white text-white font-bold text-lg'
+          : 'bg-red-500 text-white font-bold text-lg'
       } hover:bg-white hover:border-red-500 hover:text-red-500`}
       onClick={(e) => e.preventDefault()}
     >
