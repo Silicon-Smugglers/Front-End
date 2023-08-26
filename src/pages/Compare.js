972c5080f2e3fc17b6ad2335455d305029be9075
Parth Bhargava <parthbhargava1209@gmail.com>
2023-08-27 06:07:50 +1000

    Update drug display and compare feature on compare page with backend integration

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -1,107 +1,115 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import './css/Compare.css'; // Import your custom CSS for styling
 import './css/SearchBar.css'
 
 const Compare = () => {
-  const drugData = [
-    'Aspirin',
-    'Ibuprofen',
-    'Acetaminophen',
-    'Lisinopril',
-    'Simvastatin',
-    'Metformin',
-    'Valproic Acid',
-    'Benzodiazepines',
-    'Tylenol',
-    'Vitamin K',
-    'Warfarin',
-    'Oral Contraceptives',
-    'NSAIDs'
-    // Add more drug names as needed
-  ];
+    const [allDrugs, setAllDrugs] = useState([]);
 
-  // State to hold the search query
-  const [searchQuery, setSearchQuery] = useState('');
+    useEffect(() => {
+        fetch('/list').then(res => res.json()).then(data => {
+            setAllDrugs(data);
+        });
+    }, []);
 
-  // State to hold the selected drugs in the cart
-  const [cart, setCart] = useState([]);
+    const drugData = [
+        'Aspirin',
+        'Ibuprofen',
+        'Acetaminophen',
+        'Lisinopril',
+        'Simvastatin',
+        'Metformin',
+        'Valproic Acid',
+        'Benzodiazepines',
+        'Tylenol',
+        'Vitamin K',
+        'Warfarin',
+        'Oral Contraceptives',
+        'NSAIDs'
+        // Add more drug names as needed
+    ];
 
-  // State to keep track of clicked tiles
-  const [clickedTiles, setClickedTiles] = useState([]);
+    // State to hold the search query
+    const [searchQuery, setSearchQuery] = useState('');
 
-  // Define the handleSearch function
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
+    // State to hold the selected drugs in the cart
+    const [cart, setCart] = useState([]);
 
-  // Define a function to add a drug to the cart
-  const addToCart = (drug) => {
-    // Check if the drug is already in the cart
-    if (!cart.includes(drug)) {
-      // If it's not in the cart, add it
-      setCart([...cart, drug]);
-      // Update the clickedTiles state to mark this tile as clicked
-      setClickedTiles([...clickedTiles, drug]);
-    }
-  };
+    // State to keep track of clicked tiles
+    const [clickedTiles, setClickedTiles] = useState([]);
 
-  // Define a function to remove a drug from the cart
-  const removeFromCart = (item) => {
-    const updatedCart = cart.filter((drug) => drug !== item);
-    setCart(updatedCart);
-  };
+    // Define the handleSearch function
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    };
 
-  return (
-    <div className='CompareContainer'>
-      <div className='MainContent'>
+    // Define a function to add a drug to the cart
+    const addToCart = (drug) => {
+        // Check if the drug is already in the cart
+        if (!cart.includes(drug)) {
+            // If it's not in the cart, add it
+            setCart([...cart, drug]);
+            // Update the clickedTiles state to mark this tile as clicked
+            setClickedTiles([...clickedTiles, drug]);
+        }
+    };
+
+    // Define a function to remove a drug from the cart
+    const removeFromCart = (item) => {
+        const updatedCart = cart.filter((drug) => drug !== item);
+        setCart(updatedCart);
+    };
+
+    return (
+        <div className='CompareContainer'>
+        <div className='MainContent'>
         <div className='scroll'>
-      
 
-          {/* Render the SearchBar component and pass the handleSearch function */}
-          <SearchBar onSearch={handleSearch} />
 
-          {/* Display all drugs from the list as horizontally arranged square tiles */}
-          <div className='CompareContent'>
-            <div className="tile-container">
-              {drugData.map((drug, index) => (
-                // Conditionally render only the matching tile based on the search query
-                drug.toLowerCase().includes(searchQuery.toLowerCase()) && (
-                  <div
-                    className="square-tile"
-                    key={index}
-                    onClick={() => addToCart(drug)} // Add this onClick handler
-                  >
-                    <div className="tile-content">
-                      <h5 className="tile-title">{drug}</h5>
-                      {/* Additional drug information or actions can be added here */}
-                    </div>
-                  </div>
-                )
-              ))}
-            </div>
-          </div>
+        {/* Render the SearchBar component and pass the handleSearch function */}
+        <SearchBar onSearch={handleSearch} finishedCart={cart}/>
+
+        {/* Display all drugs from the list as horizontally arranged square tiles */}
+        <div className='CompareContent'>
+        <div className="tile-container">
+        {allDrugs.map((drug, index) => (
+            // Conditionally render only the matching tile based on the search query
+            drug[1].name.toLowerCase().includes(searchQuery.toLowerCase()) && (
+                <div
+                className="square-tile"
+                key={drug[0]}
+                onClick={() => addToCart(drug)} // Add this onClick handler
+                >
+                <div className="tile-content">
+                <h5 className="tile-title">{drug[1].name}</h5>
+                {/* Additional drug information or actions can be added here */}
+                </div>
+                </div>
+            )
+        ))}
+        </div>
+        </div>
 
 
         </div>
-      </div>
+        </div>
 
-      {/* Move the cart to the right side */}
-      <div className="sidebar">
-          <h2 style={{ borderBottom: "1px black solid", marginBottom: "10px"}}>Prescriptions</h2>
-          {cart.map((item, index) => (
-            <div key={index} className="cart-item">
-              <div className="cart-content">
-                <span>{item}</span>
-                <button onClick={() => removeFromCart(item)} className="remove-button">
-                  🗑️{/* Bin symbol */}
-                </button>
-              </div>
+        {/* Move the cart to the right side */}
+        <div className="sidebar">
+        <h2 style={{ borderBottom: "1px black solid", marginBottom: "10px"}}>Prescriptions</h2>
+        {cart.map((item, index) => (
+            <div key={item[0]} className="cart-item">
+            <div className="cart-content">
+            <span>{item[1].name}</span>
+            <button onClick={() => removeFromCart(item)} className="remove-button">
+            🗑️{/* Bin symbol */}
+            </button>
             </div>
-          ))}
-      </div>
-    </div>
-  );
+            </div>
+        ))}
+        </div>
+        </div>
+    );
 };
 
 export default Compare;
--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import "./css/SearchBar.css"
 
-const SearchBar = ({ onSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+const SearchBar = ({ onSearch, finishedCart }) => {
+    const [searchQuery, setSearchQuery] = useState('');
 
-  const handleInputChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
+    const handleInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
 
-  const handleSearch = () => {
-    onSearch(searchQuery);
-  };
+    const handleSearch = () => {
+        onSearch(searchQuery);
+    };
 
-  return (
-    <div className='SearchContainer'>
-      <input
+    const handleFinish = () => {
+        // Create list of ids from list of (doc.id, doc) tuples
+        const idList = finishedCart.map(function(tuple) {
+            return tuple[0];
+        });
+        // send to server to compare
+        const data = { 'cart': idList };
+        axios.post('/compare', data)
+            .then((response) => {
+                console.log(response.data);
+            }
+        );
+    };
+
+
+    return (
+        <div className='SearchContainer'>
+        <input
         type="text"
         placeholder="Enter name of medication..."
         value={searchQuery}
         onChange={handleInputChange}
         style={{ marginRight: "15px" }}
-      />
-      <button className='SearchContainerButton' onClick={handleSearch}>Search</button>
-      <button className='SearchContainerButton'>Finish</button>
-    </div>
-  );
+        />
+        <button className='SearchContainerButton' onClick={handleSearch}>Search</button>
+        <button className='SearchContainerButton' onClick={handleFinish}>Finish</button>
+        </div>
+    );
 };
 
 export default SearchBar;
