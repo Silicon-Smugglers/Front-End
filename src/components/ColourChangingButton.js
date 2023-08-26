62a19f58f34199d7d21cacb4f17853375b927089
SillySuli <sulavmalla.malla7@gmail.com>
2023-08-27 02:53:14 +1000

    add title page

---
+++ src/components/ColourChangingButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function ColorChangingButton({
+  component: Component = 'button',
+  children,
+  className,
+  ...buttonProps
+}) {
+  return (
+    <Component
+      {...buttonProps}
+      className={`p-4 rounded-lg border transition-all duration-300 ease-in-out ${
+        buttonProps.pressed
+          ? 'bg-white border-red-500 text-red-500 font-semibold text-lg'
+          : 'bg-red-500 border-white text-white font-bold text-lg'
+      } hover:bg-white hover:border-red-500 hover:text-red-500`}
+      onClick={(e) => e.preventDefault()}
+    >
+      {children}
+    </Component>
+  );
+}
