8f68f7efb41190db2572fa191a5724b2218c2965
SillySuli <sulavmalla.malla7@gmail.com>
2023-08-26 21:34:42 +1000

    add different colour to navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       {/*Navigation Bar*/}
-      <nav class="bg-gray-800">
+      <nav class="bg-white">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div class="relative flex h-16 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
